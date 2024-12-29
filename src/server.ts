6aa6b0cfcbf593759111c30bc55d6e52d194df94
Hyeanwoo <wkdus7h@naver.com>
2024-12-29 20:18:30 +0900

    refactor: apply prettier

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
 
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' });
 });
 
-fastify.listen({ port: 8800, host: "0.0.0.0" }, function (err, address) {
+fastify.listen({ port: 8800, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
